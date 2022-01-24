0fc66e1027a8c29fc9782c8d4a606c6a8106deaa
Mgrdich <mgotm13@gmail.com>
2022-01-25 00:14:33 +0400

    Creating MainNavigator main file and importing it into the code

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import {Provider} from "react-redux";
 import store from "./store";
+import {View} from "react-native";
+import MainNavigator from "./navigation/MainNavigator";
 
 export default function App() {
   return (
     <Provider store={store}>
-
+      <MainNavigator/>
     </Provider>
   );
 }
