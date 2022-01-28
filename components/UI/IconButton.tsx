2923911a6ff3feede09fbd67bbc9afb6e1ce9783
Mgrdich <mgotm13@gmail.com>
2022-01-28 23:23:15 +0400

    Adding IconButton UI element for touchable Icons

---
+++ components/UI/IconButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {TouchableOpacity, TouchableOpacityProps} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
+
+interface IconButtonProps extends TouchableOpacityProps {
+    iconName: string,
+    iconSize: number,
+    iconColor: string
+}
+
+const IconButton: React.FC<IconButtonProps> = ({iconName, iconSize, iconColor, ...touchableOpacityProps}) => {
+    return (
+        <TouchableOpacity
+            {...touchableOpacityProps}
+        >
+            <Ionicons
+                name={iconName}
+                size={iconSize}
+                color={iconColor}
+            />
+        </TouchableOpacity>
+    );
+};
+
+export default IconButton;
