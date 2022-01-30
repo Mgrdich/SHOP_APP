d2a81d789b3b195d8233bf36020f080a10144b2d
Mgrdich <mgotm13@gmail.com>
2022-01-31 00:11:07 +0400

    adding Actions to product create edit Actions

--- store/actions/products.ts
+++ store/actions/products.ts
@@ -1,7 +1,9 @@
 import {ActionType} from "./types";
 
 export enum PRODUCTS_ACTIONS {
-    DELETE_PRODUCT = 'DELETE_PRODUCT'
+    DELETE_PRODUCT = 'DELETE_PRODUCT',
+    CREATE_PRODUCT = 'CREATE_PRODUCT',
+    EDIT_PRODUCT = 'EDIT_PRODUCT'
 }
 
 type ProductActionType = ActionType<PRODUCTS_ACTIONS>
@@ -9,6 +11,34 @@ type ProductActionType = ActionType<PRODUCTS_ACTIONS>
 export function deleteProduct(id: string): ProductActionType {
     return {
         type: PRODUCTS_ACTIONS.DELETE_PRODUCT,
-        pId:id
+        pId: id
+    }
+}
+
+// TODO check whether to refactor this
+type productType = { title: string, description: string, imageUrl: string, price: string };
+
+export function createProduct(product: productType): ProductActionType {
+    return {
+        type: PRODUCTS_ACTIONS.CREATE_PRODUCT,
+        productData: {
+            title: product.title,
+            description: product.description,
+            imageUrl: product.imageUrl,
+            price: product.price
+        }
+    }
+}
+
+export function editProduct(id: string, product: productType): ProductActionType {
+    return {
+        type: PRODUCTS_ACTIONS.EDIT_PRODUCT,
+        pId: id,
+        productData: {
+            title: product.title,
+            description: product.description,
+            imageUrl: product.imageUrl,
+            price: product.price
+        }
     }
 }
