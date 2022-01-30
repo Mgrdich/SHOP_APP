1555358ac38b76489c3faf2c78738e2c38b6a2fe
Mgrdich <mgotm13@gmail.com>
2022-01-30 23:36:40 +0400

    Adding Input Label component

---
+++ components/UI/InputLabel.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {StyleSheet, TextInput, TextInputProps, View} from "react-native";
+import StyledText from "../Styled/StyledText";
+
+interface InputLabelProps extends TextInputProps{
+    title: string
+}
+
+const InputLabel: React.FC<InputLabelProps> = ({title,textInputProps}) => {
+    return (
+        <View style={styles.formControl}>
+            <StyledText style={styles.label} bold>{title}</StyledText>
+            <TextInput {...textInputProps} style={styles.input}/>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    formControl: {
+        width: '100%'
+    },
+    label: {
+        marginVertical: 8
+    },
+    input: {
+        paddingHorizontal: 2,
+        paddingVertical: 5,
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1
+    }
+});
+
+export default InputLabel;
