edca76d8663b9745032ea9f180b0003aafac1aaf
Mgrdich <mgotm13@gmail.com>
2022-02-01 21:52:34 +0400

    Use Form Typescript linter Fix

--- hooks/useForm.ts
+++ hooks/useForm.ts
@@ -1,5 +1,4 @@
-import {SyntheticEvent, useCallback, useReducer} from "react";
-import {ActionType} from "../store/actions/types";
+import {Dispatch, ReducerAction, SyntheticEvent, useCallback, useReducer} from "react";
 
 enum USE_FORM_ACTION {
     RESET_TO_INITIAL = 'RESET_TO_INITIAL',
@@ -12,12 +11,17 @@ type useFormConfig = {
     validationRules: validationRuleType[],
 }
 
-type formStateType<T> = {
-    formData: T,
+interface State {
+    formData: {},
     errors: {}
 }
 
-function formReducer<T>(state: formStateType<T>, action: ActionType<USE_FORM_ACTION>) {
+interface Action {
+    type: USE_FORM_ACTION
+    [key: string]: any
+}
+
+function formReducer(state:State, action:Action):State {
     switch (action.type) {
         case USE_FORM_ACTION.UPDATE:
             return {
@@ -53,12 +57,15 @@ function formReducer<T>(state: formStateType<T>, action: ActionType<USE_FORM_ACT
 }
 
 
-function useForm<T>(initialState: T, config: useFormConfig) {
+function useForm(initialState, config: useFormConfig) {
 
-    const [state, dispatch] = useReducer<formStateType<T>>(formReducer, {
-        formData: initialState,
+    const initialRedState: State = {
+        formData: {},
         errors: {}
-    });
+    }
+
+    // TODO some kind of bug
+    const [state, dispatch]= useReducer(formReducer, initialRedState as any);
 
     const resetFormToInitial = useCallback(function () {
         dispatch({type: USE_FORM_ACTION.DELETE_DORM_DATA});
@@ -71,15 +78,15 @@ function useForm<T>(initialState: T, config: useFormConfig) {
     const onChangeHandler = useCallback(function (event: SyntheticEvent) {
         // couple of dispatches get thrown into a single render which is and optimization
 
-        const { name, value } = event;
+        const {name, value} = event.target;
 
         // update values
-        dispatch({type:USE_FORM_ACTION.UPDATE, name:name, value:value});
+        dispatch({type: USE_FORM_ACTION.UPDATE, name: name, value: value});
 
-        if(config.validationRules.length) {
+        if (config.validationRules.length) {
             for (const rule of config.validationRules) {
-                if(!rule.validate(value)) {
-                    dispatch({type:USE_FORM_ACTION.SET_INPUT_ERROR, name:name, value:value});
+                if (!rule.validate(value)) {
+                    dispatch({type: USE_FORM_ACTION.SET_INPUT_ERROR, name: name, value: value});
                 }
             }
         }
