9c247b48fe0a791ebc6d4e7caab3e27da13a0faa
Mgrdich <mgotm13@gmail.com>
2022-02-05 22:53:11 +0400

    Adding Configs file modifications

--- configs.ts
+++ configs.ts
@@ -1,11 +1,19 @@
-import {Dictionary} from "./types";
 import {FIREBASE_URL} from "@env";
 
-const CONFIGS: Dictionary<string> = {
+interface I_CONFIG {
+    firebase_url: string,
+    products_url: string,
+    products_url_id: string
+}
+
+const CONFIGS: I_CONFIG = {
     firebase_url: FIREBASE_URL,
     products_url: '',
+    products_url_id: ''
 };
 
 CONFIGS.products_url = `${CONFIGS.firebase_url}/products.json`;
+
+CONFIGS.products_url_id = `${CONFIGS.firebase_url}/products/{{0}}.json`
 
 export default CONFIGS;
