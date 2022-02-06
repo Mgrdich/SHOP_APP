a291df3e32fce49bac65e24150c7e7e26d835e13
Mgrdich <mgotm13@gmail.com>
2022-02-06 18:53:39 +0400

    minor Modifications in the Something Went Wrong Component

--- components/UI/SomethingWentWrong.tsx
+++ components/UI/SomethingWentWrong.tsx
@@ -6,16 +6,18 @@ import StyledButton from "../Styled/StyledButton";
 
 interface SomethingWentWrongProps {
     onPress?: Function,
+    btnText?: string,
     text?: string
 }
 
-const SomethingWentWrong: React.FC<SomethingWentWrongProps> = ({text , onPress}) => {
+const SomethingWentWrong: React.FC<SomethingWentWrongProps> = ({text, btnText, onPress}) => {
     return (
         <FullScreen>
             <StyledText bold>{text && 'Something went Wrong'}</StyledText>
             {onPress && <StyledButton style={styles.btn}
-                                      primary title="Try again"
-                                      onPress={onPress}/>
+                                      primary title={btnText}
+                                      onPress={onPress}
+            />
             }
         </FullScreen>
     );
