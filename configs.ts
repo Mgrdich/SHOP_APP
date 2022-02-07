d907536280f4844c10d73ca869d04709d398852d
Mgrdich <mgotm13@gmail.com>
2022-02-07 23:53:34 +0400

    Authentication functionality minor modificaitons

--- configs.ts
+++ configs.ts
@@ -2,11 +2,12 @@ import {FIREBASE_URL, API_KEY} from "@env";
 
 interface I_CONFIG {
     firebase_url: string,
-    api_key:string
+    api_key: string,
     products_url: string,
     products_url_id: string,
     orders_url: string,
-    orders_url_id:string,
+    orders_url_id: string,
+    auth_url: string
 }
 
 const CONFIGS: I_CONFIG = {
@@ -15,8 +16,12 @@ const CONFIGS: I_CONFIG = {
     products_url: '',
     products_url_id: '',
     orders_url: '',
-    orders_url_id: ''
+    orders_url_id: '',
+    auth_url: ''
 };
+
+CONFIGS.auth_url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${CONFIGS.api_key}`;
+console.log(CONFIGS.auth_url);
 
 CONFIGS.products_url = `${CONFIGS.firebase_url}/products.json`;
 
--- store/actions/auth.ts
+++ store/actions/auth.ts
@@ -1,3 +1,6 @@
+import FunctionUtil from "../../util/FunctionUtil";
+import CONFIGS from "../../configs";
+
 export enum AUTH_ACTIONS {
     SIGN_UP = 'SIGN_UP',
     LOGIN = 'LOGIN'
@@ -5,12 +8,36 @@ export enum AUTH_ACTIONS {
 
 export const signup = (email: string, password: string) => {
     return async (dispatch) => {
+        try {
+            let res = await FunctionUtil.post(CONFIGS.auth_url, {
+                email,
+                password,
+                returnSecureToken: true
+            });
 
+            dispatch({
+                type : AUTH_ACTIONS.SIGN_UP
+            });
+        } catch (e) {
+            throw Error('Something Went Wrong');
+        }
     }
 }
 
 export const login = (email: string, password: string) => {
     return async (dispatch) => {
+        try {
+            let res = await FunctionUtil.get(CONFIGS.auth_url, {
+                email,
+                password,
+                returnSecureToken: true
+            });
 
+            dispatch({
+                type : AUTH_ACTIONS.LOGIN
+            });
+        } catch (e) {
+            throw Error('Something Went Wrong');
+        }
     }
 }
