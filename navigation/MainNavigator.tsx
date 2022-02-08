12d689064f2ac566d3da8a8f5cc894cb3c217724
Mgrdich <mgotm13@gmail.com>
2022-02-08 22:21:22 +0400

    Authentication Main Navigator hiding pages if not authenticated

--- navigation/MainNavigator.tsx
+++ navigation/MainNavigator.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import DrawerNavigator from "./DrawerNavigator";
+import {useAppSelector} from "../hooks/redux";
+import AuthNavigator from "./AuthNavigator";
 
 const MainNavigator: React.FC = () => {
+    const isAuth: boolean = useAppSelector(state => state.auth.userId);
+
     return (
         <NavigationContainer>
-            <DrawerNavigator/>
+            {
+                isAuth ? <DrawerNavigator/> : <AuthNavigator/>
+            }
         </NavigationContainer>
     )
 };
