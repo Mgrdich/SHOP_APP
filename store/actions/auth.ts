81ae6c154947fdd5388997bf2eeb2cde983d2ae1
Mgrdich <mgotm13@gmail.com>
2022-02-09 23:07:19 +0400

    Finalizing the Authentication Component

--- store/actions/auth.ts
+++ store/actions/auth.ts
@@ -1,3 +1,4 @@
+import {AsyncStorage} from "react-native";
 import FunctionUtil from "../../util/FunctionUtil";
 import CONFIGS from "../../configs";
 
@@ -6,43 +7,10 @@ export enum AUTH_ACTIONS {
     LOGOUT = 'LOGOUT'
 }
 
-export const signup = (email: string, password: string) => {
+function auth(email: string, password: string, url: string):Function {
     return async (dispatch) => {
         try {
-            let res = await FunctionUtil.post(CONFIGS.auth_signup_url, {
-                email,
-                password,
-                returnSecureToken: true
-            });
-
-            if (res.error) {
-               let message:string;
-                console.log(res.error);
-               if(res.error.message === 'EMAIL_NOT_FOUND') {
-                   console.log('s');
-                   message = 'Email is Not Found'
-               } else if(res.error.message === 'INVALID_PASSWORD') {
-                   message = 'Invalid Password'
-               }
-               return Promise.reject(message || 'Something Went Wrong');
-            }
-
-
-            return dispatch({
-                type : AUTH_ACTIONS.AUTHENTICATE,
-                token: res['idToken'],
-                userId: res['localId']
-            });
-        } catch (e) {
-            throw Error('Something Went Wrong');
-        }
-    }
-}
-
-export const login = (email: string, password: string) => {
-    return async (dispatch) => {
-        try {
-            let res = await FunctionUtil.post(CONFIGS.auth_signin_url, {
+            let res = await FunctionUtil.post(url, {
                 email,
                 password,
                 returnSecureToken: true
@@ -58,8 +26,9 @@ export const login = (email: string, password: string) => {
                 return Promise.reject(message || 'Something Went Wrong');
             }
 
+
             return dispatch({
-                type : AUTH_ACTIONS.AUTHENTICATE,
+                type: AUTH_ACTIONS.AUTHENTICATE,
                 token: res['idToken'],
                 userId: res['localId']
             });
@@ -67,4 +36,13 @@ export const login = (email: string, password: string) => {
             throw Error('Something Went Wrong');
         }
     }
+}
+
+
+export const signup = (email: string, password: string) => {
+    return auth(email, password ,CONFIGS.auth_signup_url);
+}
+
+export const login = (email: string, password: string) => {
+    return auth(email, password ,CONFIGS.auth_signin_url);
 }
